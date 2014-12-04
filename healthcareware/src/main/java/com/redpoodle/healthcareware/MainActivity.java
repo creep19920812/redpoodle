dcdaea9c47b2b5bc73aa20561068aeaa72f6c994
redpoodle.supervisor <redpoodle.supervisor@gmail.com>
2014-12-04 13:32:26 +0000

    バージョン２開発開始

--- healthcareware/src/main/java/com/redpoodle/healthcareware/MainActivity.java
+++ healthcareware/src/main/java/com/redpoodle/healthcareware/MainActivity.java
@@ -1,126 +1,42 @@
 package com.redpoodle.healthcareware;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.os.SystemClock;
 import android.os.Vibrator;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
+
+import com.googlecode.androidannotations.annotations.ViewById;
 
 public class MainActivity extends Activity {
 
-	private Button startButton;
-	private Button stopButton;
-	private Button resetButton;
-	private Button set1;
-	private Button set2;
-	private Button set3;
-	private Button set4;
-	private Chronometer chronometer;
-	private long time = 0;
+	// メッセージボックス
+	@ViewById(R.id.rscMsgBox)
+	private TextView tvMsgBox;
 	
-	private boolean isComplateSet1;
-	private boolean isComplateSet2;
-	private boolean isComplateSet3;
-	private boolean isComplateSet4;
+	// インターバルタイマー
+	@ViewById(R.id.rscIntervalTimer)
+	private TextView tvIntervalTimer;
+	
+	// セット１
+	@ViewById(R.id.rscSet1)
+	private TextView tvSet1;
+
+	// セット２
+	@ViewById(R.id.rscSet2)
+	private TextView tvSet2;
+
+	// セット３
+	@ViewById(R.id.rscSet3)
+	private TextView tvSet3;
+
+	// セット４
+	@ViewById(R.id.rscSet4)
+	private TextView tvSet4;
 	
 	private static final long VIB_TIME = 50;
 
 	private Vibrator vib;
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		// View�̏���
-		chronometer = (Chronometer) findViewById(R.id.chronometer);
-		startButton = (Button) findViewById(R.id.startButton);
-		stopButton = (Button) findViewById(R.id.stopButton);
-		resetButton = (Button) findViewById(R.id.resetButton);
-		set1 = (Button) findViewById(R.id.toggleButton1);
-		set2 = (Button) findViewById(R.id.toggleButton2);
-		set3 = (Button) findViewById(R.id.toggleButton3);
-		set4 = (Button) findViewById(R.id.toggleButton4);
-		
-		vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
-		
-		stopButton.setEnabled(false);
-
-		// �{�^���������ꂽ���̏���
-		startButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// �X�^�[�g
-				vib.vibrate(VIB_TIME);
-				startButton.setEnabled(false);
-				stopButton.setEnabled(true);
-				chronometer.setBase(SystemClock.elapsedRealtime() - time);
-				chronometer.start();
-			}
-		});
-		stopButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// �X�g�b�v
-				vib.vibrate(VIB_TIME);
-				startButton.setEnabled(true);
-				stopButton.setEnabled(false);
-				chronometer.stop();
-				time = SystemClock.elapsedRealtime() - chronometer.getBase();
-			}
-		});
-		resetButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// ���Z�b�g
-				vib.vibrate(VIB_TIME);
-				startButton.setEnabled(true);
-				stopButton.setEnabled(false);
-				chronometer.stop();
-				chronometer.setBase(SystemClock.elapsedRealtime());
-				time = 0;
-			}
-		});
-		set1.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vib.vibrate(VIB_TIME);
-			}
-		});
-		set2.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vib.vibrate(VIB_TIME);
-			}
-		});
-		set3.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vib.vibrate(VIB_TIME);
-			}
-		});
-		set4.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				vib.vibrate(VIB_TIME);
-			}
-		});
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 
 }
